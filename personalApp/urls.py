4295a29c15bced9ac9179aba77c3dabfe12d6028
esadakman <esadd26@gmail.com>
2022-10-11 22:07:13 +0300

    views

---
+++ personalApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import DepartmentView,DepartmentPersonalView
+
+
+urlpatterns = [
+    path('', DepartmentView.as_view()),
+    path('department/<str:department>/', DepartmentPersonalView.as_view()),
+]
--- personalApp/views.py
+++ personalApp/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework import generics
+from .models import Department, Personal
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import DepartmentPersonalSerializer, DepartmentSerializer, PersonalSerializer
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class DepartmentView(generics.ListAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    # permission_classes = [IsAuthenticated]
+
+
+class DepartmentPersonalView(generics.ListAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+
+    def get_queryset(self):
+        name = self.kwargs['department']
+        return Department.objects.filter(name__iexact=name)
+
+
+class PersonalListCreate(ListCreateAPIView):
+    queryset = Personal.objects.all()
+    serializer_class=PersonalSerializer
+    # permission_classes = [IsAuthenticated]
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if self.request.user.is_superuser or self.request.user.is_staff:#sadece ilgili kullanıcı staff veya superuser ise personel create işlemi yapabilir
+            self.perform_create(serializer)
+            data = {
+                "message": f"Personal {serializer.validated_data.get('first_name')} saved successfully!"}
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            data = {
+                "message": "You are not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+    def perform_create(self, serializer):
+        serializer.save()
